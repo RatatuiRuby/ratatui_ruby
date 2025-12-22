c87ef5ba73796069f3479db13f87ea342796225c
Kerrick Long <me@kerricklong.com>
2025-12-22 00:44:15 -0600

    Initialize Cargo

---
+++ ext/ratatui_ruby/src/lib.rs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 Kerrick Long <me@kerricklong.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
